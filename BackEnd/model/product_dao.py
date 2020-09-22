a0779287b71def351689578bbc874e51f1b51f39
KIM TAE SOO <blakekim93@gmail.com>
2020-09-22 10:24:48 +0900

    Modify: DB connection 수정
    - 클래스에서 하던 것을 함수마다 connect하고 close 하도록 구현

--- BackEnd/model/product_dao.py
+++ BackEnd/model/product_dao.py
@@ -7,29 +7,30 @@ from connection import get_connection
 # Product와 연결된 Class
 class ProductDao:
     def __init__(self, db):
-        self.database = get_connection(db)
-        self.cursor   = self.database.cursor(pymysql.cursors.DictCursor)
+        self.db = db
 
     # 작성자: 김태수
     # 작성일: 2020.09.21.월
     # 원산지 데이터를 데이터베이스에서 가져오는 함수
     def get_country_of_origin(self, country_id):
         try:
+            db     = get_connection(self.db)
+            cursor = db.cursor(pymysql.cursors.DictCursor)
+
             sql = """
             SELECT id, name
             FROM tests
             WHERE id = %s;
             """
 
-            self.cursor.execute(sql, country_id)
-            result = self.cursor.fetchone()
+            cursor.execute(sql, country_id)
+            result = cursor.fetchone()
         except:
-            self.database.rollback()
-            self.cursor.close()
-            self.database.close()
+            db.rollback()
             raise
         else:
-            self.database.commit()
-            self.cursor.close()
-            self.database.close()
+            db.commit()
             return result if result else None
+        finally:
+            cursor.close()
+            db.close()
