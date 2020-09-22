6ab24bddf7c4bc672e72b5dd6a1e8ea236edeea5
Choonghee Lee <choonghee.dev@gmail.com>
2020-09-22 16:29:49 +0900

    Add: [상품 등록] 셀러 검색 엔드포인트 구현 (#9)
    - 쿼리스트링으로 검색어와 limit 값으로 셀러 이름을 검색하여 가져온다.

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -3,8 +3,8 @@ import pymysql
 from flask      import Flask
 from flask_cors import CORS
 
-from model   import ProductDao
-from service import ProductService
+from model   import ProductDao, SellerDao
+from service import ProductService, SellerService
 from view    import create_endpoints
 
 import config
@@ -26,10 +26,12 @@ def create_app(test_config = None):
     db = config.database
 
     product_dao = ProductDao(db)
+    seller_dao = SellerDao(db)
 
     services = Services
 
     services.product_service = ProductService(product_dao, app.config)
+    services.seller_service  = SellerService(seller_dao, app.config)
 
     create_endpoints(app, services)
 
--- BackEnd/service/__init__.py
+++ BackEnd/service/__init__.py
@@ -1,5 +1,7 @@
 from .product_service import ProductService
+from .seller_service  import SellerService
 
 __all__ = [
-    ProductService
+    ProductService,
+    SellerService
 ]
